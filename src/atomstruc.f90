8ef1527a561f315b0f3fb2dcffc9f076003783dc
Ned Taylor <71959356+nedtaylor@users.noreply.github.com>
2025-10-10 09:19:58 +0100

    Add top-level file

--- src/atomstruc.f90
+++ src/atomstruc.f90
@@ -1,10 +1,13 @@
 module atomstruc
-  implicit none
-  private
+  !! This is the top-level module for the atomstruc Fortran library.
+  use atomstruc__types, only: basis_type, species_type
+  use atomstruc__io, only: geom_read, geom_write, igeom_input, igeom_output
+  use atomstruc__elements, only: get_element_properties
+  use atomstruc__utils, only: basis_merge
+  use atomstruc__extd, only: extended_basis_type
 
-  public :: say_hello
-contains
-  subroutine say_hello
-    print *, "Hello, atomstruc!"
-  end subroutine say_hello
+  implicit none
+
+  public
+
 end module atomstruc
